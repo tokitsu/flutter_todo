a836c2cb2948cfaf4248cbb3abd0a99e756065a0
tokitsu <toki2222son@yahoo.co.jp>
2021-08-01 19:41:49 +0900

    init

---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Default'),
+            // 太さを指定
+            Text('Bold', style: TextStyle(fontWeight: FontWeight.bold)),
+            // スタイルを指定
+            Text('Italic', style: TextStyle(fontStyle: FontStyle.italic)),
+            // サイズを指定
+            Text('fontSize = 36', style: TextStyle(fontSize: 36)),
+            // 色を指定
+            Text('Red', style: TextStyle(color: Colors.red,fontSize: 36)),
+            Container(
+              width: double.infinity,
+              color: Colors.grey,
+              // 表示位置を指定
+              child: Text('TextAlign.center', textAlign: TextAlign.center),
+            ),
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
